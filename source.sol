071dea8af80813b366a931fac45ef5b83b5df339
Mr.MAVERICK <140925628+Mr-mutneir48@users.noreply.github.com>
2023-08-05 16:41:46 +0530

    Update source.sol

--- source.sol
+++ source.sol
@@ -1,1 +1,296 @@
+pragma solidity ^0.5.0;
 
+contract MedicalChain {
+
+    // ------------------ Patient Registration and Identity Verification ------------------
+
+    // Mapping to store verified patients
+    mapping(address => bool) private verifiedPatients;
+
+    // Event for patient registration
+    event PatientRegistered(address patient);
+
+    // Function to register a patient and verify their identity
+    function registerPatient() public {
+        verifiedPatients[msg.sender] = true;
+        emit PatientRegistered(msg.sender);
+    }
+
+    // Modifier to check if the patient is verified
+    modifier Only_Patients() {
+        require(verifiedPatients[msg.sender], "Only verified patients allowed.");
+        _;
+    }
+
+    // ------------------ Medical Records Upload and Storage ------------------
+
+    // Mapping to store medical records for patients
+    mapping(address => string[]) private medicalRecords;
+
+    // Event for medical records upload
+    event MedicalRecordUploaded(address patient, string record);
+
+    // Function to upload a medical record
+    function uploadMedicalRecord(string memory record) public Only_Patients {
+        medicalRecords[msg.sender].push(record);
+        emit MedicalRecordUploaded(msg.sender, record);
+    }
+
+    // Function to get the count of medical records for a patient
+    function getMedicalRecordCount(address patient) public view returns (uint) {
+        return medicalRecords[patient].length;
+    }
+
+    // Function to get a specific medical record for a patient
+    function getMedicalRecord(address patient, uint index) public view returns (string memory) {
+        require(index < medicalRecords[patient].length, "Invalid record index.");
+        return medicalRecords[patient][index];
+    }
+
+    // ------------------ Data Sharing Permissions ------------------
+
+    // Mapping for data sharing permissions
+    mapping(address => mapping(address => bool)) private dataAccess;
+
+    // Function to set data sharing permission for a patient
+    function setAccess(address patient, bool access) public Only_Patients {
+        dataAccess[patient][msg.sender] = access;
+    }
+
+    // Function to check if a doctor has access to a patient's data
+    function hasAccess(address patient, address doctor) public view returns (bool) {
+        return dataAccess[patient][doctor];
+    }
+
+    // ------------------ Interoperability with Healthcare Providers ------------------
+
+    // Mapping for healthcare providers' systems
+    mapping(address => string) private providerData;
+
+    // Function to integrate with a healthcare provider's system
+    function integrateWithProvider(string memory providerSystemData) public {
+        require(isDoctor(msg.sender), "Only registered doctors can integrate with providers.");
+        providerData[msg.sender] = providerSystemData;
+    }
+
+    // Function to fetch data from a healthcare provider's system
+    function getProviderData(address doctor) public view returns (string memory) {
+        return providerData[doctor];
+    }
+
+    // ------------------ Access Control and Audit Trails ------------------
+
+    // Mapping for access requests
+    mapping(address => mapping(address => bool)) private accessRequests;
+
+    // Mapping for audit trails
+    mapping(address => string[]) private auditTrails;
+
+    // Function to request access to a patient's data
+    function requestAccess(address patient) public {
+        require(isDoctor(msg.sender), "Only registered doctors can request access.");
+        accessRequests[patient][msg.sender] = true;
+    }
+
+    // Function to grant access to a doctor
+    function grantAccess(address patient) public Only_Patients {
+        require(accessRequests[msg.sender][patient], "Access request not found.");
+        dataAccess[patient][msg.sender] = true;
+        delete accessRequests[msg.sender][patient]; // Remove the access request
+    }
+
+    // Function to log an action in the audit trail
+    function logAction(string memory action) private {
+        auditTrails[msg.sender].push(action);
+    }
+
+    // Function to get audit trail for a user
+    function getAuditTrail(address user) public view returns (string[] memory) {
+        require(msg.sender == user || (isDoctor(msg.sender) && hasAccess(user, msg.sender)),
+            "Access denied to audit trail.");
+        return auditTrails[user];
+    }
+
+    // ------------------ Health Alerts and Reminders ------------------
+
+    // Mapping for health reminders
+    mapping(address => string) private healthReminders;
+
+    // Function to set health reminder for a patient
+    function setHealthReminder(string memory reminder) public Only_Patients {
+        healthReminders[msg.sender] = reminder;
+    }
+
+    // Function to get health reminder for a patient
+    function getHealthReminder() public view returns (string memory) {
+        return healthReminders[msg.sender];
+    }
+
+    // ------------------ Telemedicine Integration ------------------
+
+    // Mapping for telemedicine appointments
+    mapping(address => mapping(address => bool)) private telemedicineAppointments;
+
+    // Function to request a telemedicine appointment
+    function requestTelemedicine(address doctor) public Only_Patients {
+        telemedicineAppointments[msg.sender][doctor] = true;
+    }
+
+    // Function to share medical records during a telemedicine appointment
+    function shareMedicalRecords(address doctor) public Only_Patients {
+        require(telemedicineAppointments[msg.sender][doctor], "No telemedicine appointment.");
+        dataAccess[msg.sender][doctor] = true;
+    }
+
+    // ------------------ Personal Health Analytics ------------------
+
+    // Struct to track health analytics
+    struct HealthAnalytics {
+        uint heartRate;
+        uint bloodPressure;
+        // Add more fields as needed
+    }
+
+    // Mapping to store health analytics
+    mapping(address => HealthAnalytics) private patientAnalytics;
+
+    // Function to track health analytics
+    function trackHealthAnalytics(uint heartRate, uint bloodPressure) public Only_Patients {
+        HealthAnalytics storage analytics = patientAnalytics[msg.sender];
+        analytics.heartRate = heartRate;
+        analytics.bloodPressure = bloodPressure;
+        // Update other fields if needed
+    }
+
+    // Function to get health analytics for a patient
+    function getHealthAnalytics() public view returns (uint heartRate, uint bloodPressure) {
+        HealthAnalytics storage analytics = patientAnalytics[msg.sender];
+        heartRate = analytics.heartRate;
+        bloodPressure = analytics.bloodPressure;
+        // Return other fields if needed
+    }
+
+    // ------------------ Medical Research Contribution ------------------
+
+    // Mapping for medical research contributions
+    mapping(address => bool) private researchContributors;
+
+    // Event for medical research contribution
+    event MedicalResearchContribution(address contributor);
+
+    // Function to opt-in for medical research contribution
+    function optInForResearchContribution() public Only_Patients {
+        researchContributors[msg.sender] = true;
+        emit MedicalResearchContribution(msg.sender);
+    }
+
+    // ------------------ Health Insurance Integration ------------------
+
+    // Mapping for health insurance claims
+    mapping(address => mapping(address => uint)) private insuranceClaims;
+
+    // Function to file a health insurance claim
+    function fileInsuranceClaim(address insuranceProvider, uint claimAmount) public Only_Patients {
+        insuranceClaims[msg.sender][insuranceProvider] = claimAmount;
+    }
+
+    // Function to verify insurance claim by provider
+    function verifyInsuranceClaim(address patient, address insuranceProvider) public Only_Patients {
+        insuranceClaims[patient][insuranceProvider] = 0; // Clear claim amount after verification
+    }
+
+    // Function to get the status of an insurance claim
+    function getInsuranceClaimStatus(address patient, address insuranceProvider) public view returns (uint) {
+        return insuranceClaims[patient][insuranceProvider];
+    }
+
+    // ------------------ Health Record Portability ------------------
+
+    // Mapping for downloaded medical records
+    mapping(address => string[]) private downloadedRecords;
+
+    // Function to download medical records
+    function downloadMedicalRecords() public Only_Patients {
+        string[] storage records = medicalRecords[msg.sender];
+        for (uint i = 0; i < records.length; i++) {
+            downloadedRecords[msg.sender].push(records[i]);
+        }
+    }
+
+    // Function to get downloaded medical records
+    function getDownloadedRecords() public view returns (string[] memory) {
+        return downloadedRecords[msg.sender];
+    }
+
+    // ------------------ Multi-Factor Authentication (MFA) ------------------
+
+    // Mapping for MFA codes
+    mapping(address => uint) private mfaCodes;
+
+    // Function to generate and set MFA code for a user
+    function generateMFA() public {
+        uint code = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, blockhash(block.number - 1))));
+        mfaCodes[msg.sender] = code;
+    }
+
+    // Function to validate MFA code
+    function validateMFA(uint code) public view returns (bool) {
+        return code == mfaCodes[msg.sender];
+    }
+
+    // ------------------ Medical Emergency Access ------------------
+
+    // Mapping for emergency access
+    mapping(address => address) private emergencyAccessDoctors;
+
+    // Function to grant emergency access to a doctor
+    function grantEmergencyAccess(address doctor) public Only_Patients {
+        emergencyAccessDoctors[msg.sender] = doctor;
+    }
+
+    // Function to access medical records during emergencies
+    function getEmergencyMedicalRecords() public view returns (string[] memory) {
+        require(emergencyAccessDoctors[msg.sender] != address(0), "Emergency access not granted.");
+        return medicalRecords[msg.sender];
+    }
+
+    // ------------------ Data Backup and Redundancy ------------------
+
+    // Mapping for data backups
+    mapping(address => string[]) private dataBackups;
+
+    // Function to backup medical data
+    function backupMedicalData(string memory data) public Only_Patients {
+        dataBackups[msg.sender].push(data);
+    }
+
+    // Function to restore medical data from backups
+    function restoreMedicalData(uint index) public Only_Patients {
+        require(index < dataBackups[msg.sender].length, "Invalid backup index.");
+        medicalRecords[msg.sender].push(dataBackups[msg.sender][index]);
+    }
+
+    // ------------------ Multi-Language Support ------------------
+
+    // Mapping for multi-language support
+    mapping(address => mapping(bytes32 => string)) private multiLanguageData;
+
+    // Function to set data for a specific language
+    function setMultiLanguageData(bytes32 language, string memory data) public {
+        multiLanguageData[msg.sender][language] = data;
+    }
+
+    // Function to get data for a specific language
+    function getMultiLanguageData(bytes32 language) public view returns (string memory) {
+        return multiLanguageData[msg.sender][language];
+    }
+
+    // ------------------ Other Helper Functions ------------------
+
+    // Function to check if an address is a registered doctor
+    function isDoctor(address account) private view returns (bool) {
+        return verifiedDoctors[account];
+    }
+
+    // Rest of your contract's functions...
+}
